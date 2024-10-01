f0d9d3ac6111dd934f36fe8159e2a6b05010c168
deepak_gophers <rayandeepak78@gmail.com>
2024-10-01 23:59:48 +0530

    My Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+/*===>WE have to see how our project start using npm we did npm init and when we did npm init then 
+ package.jason is created and package.jason file is a file for a npm configuration ,packege.jason has 
+ maintais the version of the dependency but not the exact version it have a carate(^) that track the
+ minor upgrade and the tild(~) which track major upgrade but it is alsways better to use ^ ,here
+ we can maintains the exact version by simply removing the ^ and~ and simply use in parcel version ""
+ 
+ And package-lock.jason contains the exact version of all the transitive or all the depedency that my 
+ project will ever need,node_ modules contains all the code of it and gitignore contains all the files
+ which i cannot push inside the github.
+ */
